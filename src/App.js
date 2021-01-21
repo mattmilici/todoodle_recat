efeaca86a6f6574ee22a7d4f6d1d51885817bda5
Matthew Milici <milici.m915@gmail.com>
2021-01-21 08:58:02 -0800

    changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import React, { useState } from "react";
 import "./App.css";
 import TimerComponent from "../src/components/Timer/index";
 import Typical from "react-typical";
 
 function App() {
+	const [notified, setnotified] = React.useState(true);
+
+	function changeState() {
+		setnotified(false);
+	}
 	return (
 		<div className="bg-white p-6 md:p-20 text-white h-screen w-full">
 			<div className=" mx-1 my-12 md:mx-32 md:my-12 lg:mx-56 lg:my-24">
@@ -15,7 +21,7 @@ function App() {
 				<p id="animated Text" className=" mb-12 text-xl md:text-2xl">
 					I need to{" "}
 					<Typical
-						className="text-xl  text-2xl "
+						className="text-xl md:text-2xl "
 						loop={Infinity}
 						wrapper="r"
 						steps={[
@@ -35,27 +41,39 @@ function App() {
 				<h1 className="mb-3 text-xl">you've got time</h1>
 				<TimerComponent />
 				<div className="mt-8 sm:max-w-lg sm:mx-auto sm:text-center lg:text-left lg:mx-0">
-					<p className="text-base font-medium  font-roboto">
-						Sign up to get notified when it’s ready.
-					</p>
-					<form name="contact" method="post" className="mt-3 sm:flex">
-						<label for="email" className="sr-only">
-							Email
-						</label>
-						<input
-							type="text"
-							name="email"
-							id="email"
-							className="border-todoodleBlack block w-full py-3 pl-2 rounded-md placeholder-gray-800 shadow-sm sm:flex-1 "
-							placeholder="Enter your email"
-						/>
-						<button
-							type="submit"
-							className="mt-3 w-full px-6 py-3 border text-todoodleWhite border-transparent text-base font-medium rounded-md  bg-todoodleBlack shadow-sm hover:bg-gray-800 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
-						>
-							Notify me
-						</button>
-					</form>
+					{notified ? (
+						<div>
+							<p className="text-base font-medium  font-roboto">
+								Sign up to get notified when it’s ready.
+							</p>
+							<form name="contact" method="post" className="mt-3 sm:flex">
+								<label for="email" className="sr-only">
+									Email
+								</label>
+								<input
+									type="text"
+									name="email"
+									id="email"
+									className="border-todoodleBlack block w-full py-3 pl-2 rounded-md placeholder-gray-800 shadow-sm sm:flex-1 "
+									placeholder="Enter your email"
+								/>
+								<button
+									onClick={changeState}
+									type="submit"
+									className="mt-3 w-full px-6 py-3 border text-todoodleWhite border-transparent text-base font-medium rounded-md  bg-todoodleBlack shadow-sm hover:bg-gray-800 sm:mt-0 sm:ml-3 sm:flex-shrink-0 sm:inline-flex sm:items-center sm:w-auto"
+								>
+									Notify me
+								</button>
+							</form>
+						</div>
+					) : (
+						<div>
+							<h1 className="text-xl">
+								You're all set! We'll reach out when the app is ready to go!
+							</h1>
+						</div>
+					)}
+
 					<p className="mt-3 text-sm ">
 						We care about the protection of your data. Read our{" "}
 						<a href="#" className="font-medium text-white-900 underline">
